93dc7cc3160b019ad12679fa6ceab74157fad98e
Juanes07 <juanesdm3@gmail.com>
2022-02-09 12:55:23 -0500

    Se agregan clase Main e Info
    Clase main donde se va a controlar el software. y clase info donde se crea la primer lista de canciones
    Adicional se hace prueba para comproboar filtro de genero y año

---
+++ library/Info.java
@@ -0,0 +1,110 @@
+package library;
+
+import java.util.Date;
+import java.util.stream.Stream;
+
+/**
+ *
+ * @author JUANES
+ */
+public  class  Info implements Comparable<Info> {
+
+    public String artist;
+    public String title;
+    public Integer songId;
+    public Long year;
+    public Float duration;
+    public String gender;
+    public String cover;
+    public String description;
+    String[] example = {};
+
+    public Info(String artist, String title, int songId, long year, float duration, String gender, String cover, String description) {
+        this.artist = artist;
+        this.title = title;
+        this.songId = songId;
+        this.year = year;
+        this.duration = duration;
+        this.gender = gender;
+        this.cover = cover;
+        this.description = description;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getSongid() {
+        return songId;
+    }
+
+    public void setSongid(int Songid) {
+        this.songId = Songid;
+    }
+
+    public long getYear() {
+        return year;
+    }
+
+    public void setYear(long year) {
+        this.year = year;
+    }
+
+    public float getDuration() {
+        return duration;
+    }
+
+    public void setDuration(float duration) {
+        this.duration = duration;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getCover() {
+        return cover;
+    }
+
+    public void setCover(String cover) {
+        this.cover = cover;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Info [artist= " + artist + ", title= " + title + ", songId= "
+                + songId + ", year=" + year + ", duration= " + duration + ", gender= "
+                + gender + ", cover= " + cover + ", description= " + description + "]" + "\n";
+    }  
+
+    @Override
+    public int compareTo(Info o) {
+        return year.compareTo(o.getYear());
+        
+    }  
+    
+}
---
+++ library/Main.java
@@ -0,0 +1,50 @@
+package library;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ *
+ * @author JUANES
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        List<Info> infoList = new ArrayList<>();
+        infoList.add(new Info("muse", "paradise", 1, 2015, 2.11f, "reggae", "memories.jpg", "Cancion 1 del album memories"));
+        infoList.add(new Info("metallica", "resistance", 2, 2015, 1.11f, "reggae", "memories.jpg", "Cancion 2 del album memories"));
+        infoList.add(new Info("lamda", "madness", 3, 2003, 3.12f, "b", "memories.jpg", "Cancion 3 del album memories"));
+        infoList.add(new Info("muse", "knights", 4, 2001, 4.11f, "a", "memories.jpg", "Cancion 4 del album memories"));
+        infoList.add(new Info("muse", "knights", 4, 2001, 4.11f, "a", "memories.jpg", "Cancion 4 del album memories"));
+        String[] genderList = {"rock", "punk", "rock", "jazz", "reggae"};
+
+        int opcion1;
+        Scanner scanner = new Scanner(System.in);
+        int opcion2;
+//        System.out.println("De que año deseas traer musica? Ingresa el numero el año. desde 2000 hasta 2022");
+//        opcion1 = scanner.nextInt(); 
+//        infoList.stream().filter(l -> l.getYear() == opcion1).forEach(System.out::println);
+        System.out.println("****************");
+        System.out.println("Que genero deseas filtrar: 1)rock 2)reggae 3) jazz 4)punk. Ingresa el numero correspondiente");
+        opcion2 = scanner.nextInt();
+        infoList.stream().filter(l -> l.getGender() == genderList[opcion2 - 1]).forEach(System.out::println);
+
+        int opcion3;
+
+        Collections.sort(infoList);
+        for (Info elemento : infoList) {
+            System.out.println(elemento);
+
+        }
+
+        System.out.println("****************");
+
+        Collections.sort(infoList, Collections.reverseOrder()); // mayor a menor duracion
+        for (Info elemento : infoList) {
+            System.out.println(elemento);
+        }
+
+    }
+}
