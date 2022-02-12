27e3a53e185596df5770205cdcb8a0a0b47201c5
Juanes07 <juanesdm3@gmail.com>
2022-02-11 22:56:28 -0500

    Recuperacion de datos
    Se recuperan las clases ya realizadas con documentacion

--- library/Info.java
+++ library/Info.java
@@ -2,27 +2,37 @@ package library;
 
 import java.util.Date;
 import java.util.stream.Stream;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 /**
+ * [la clase Info nos apoya como clase base abstracta. de esta manera podemos
+ * extenderla para crear otro tipo de arraysList sea con los mismos parametros u
+ * omitiendo alguno de estos.
  *
- * @author JUANES
+ *
+ * @version [1.0.000 2022-02-10]
+ *
+ * @author [Juan Esteban, Velasquez Posada ]
+ *
+ * @since [1.0.000 2022-02-10]
+ *
  */
-public  class  Info implements Comparable<Info> {
+public abstract class Info {
 
     public String artist;
     public String title;
-    public Integer songId;
-    public Long year;
-    public Float duration;
+    public int songId;
+    public int year;
+    public float duration;
     public String gender;
     public String cover;
     public String description;
-    String[] example = {};
 
-    public Info(String artist, String title, int songId, long year, float duration, String gender, String cover, String description) {
-        this.artist = artist;
-        this.title = title;
+    public Info() {
         this.songId = songId;
+        this.title = title;
+        this.artist = artist;
         this.year = year;
         this.duration = duration;
         this.gender = gender;
@@ -30,81 +40,168 @@ public  class  Info implements Comparable<Info> {
         this.description = description;
     }
 
+    /**
+     * Refleja el Id de la cancion al agregarlo en el arrayList
+     *
+     * @return el Id de la cancion
+     */
+    public int getSongId() {
+        return songId;
+    }
+
+    /**
+     * Refleja el Id de la cancion, en caso de ser seteado por usuario
+     *
+     * @param songId Id de la cancion asignado por usuario
+     */
+    public void setSongId(int songId) {
+        this.songId = songId;
+    }
+
+    /**
+     * Refleja el año de la cancion al agregarlo en el arrayList
+     *
+     * @return el año de la cancion
+     */
+    public int getYear() {
+        return year;
+    }
+
+    /**
+     * Refleja el año de la cancion, en caso de ser seteado por usuario
+     *
+     * @param year año de la cancion asignado por usuario
+     */
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    /**
+     * Refleja el artista de la cancion al agregarlo en el arrayList
+     *
+     * @return el artista de la cancion
+     */
     public String getArtist() {
         return artist;
     }
 
+    /**
+     * Refleja el artista de la cancion, en caso de ser seteado por usuario
+     *
+     * @param artist artista de la cancion asignado por usuario
+     */
     public void setArtist(String artist) {
         this.artist = artist;
     }
 
+    /**
+     * Refleja el titulo de la cancion al agregarlo en el arrayList
+     *
+     * @return el titulo de la cancion
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * Refleja el titulo de la cancion, en caso de ser seteado por usuario
+     *
+     * @param title titulo de la cancion asignado por usuario
+     */
     public void setTitle(String title) {
         this.title = title;
     }
 
-    public int getSongid() {
-        return songId;
-    }
-
-    public void setSongid(int Songid) {
-        this.songId = Songid;
-    }
-
-    public long getYear() {
-        return year;
-    }
-
-    public void setYear(long year) {
-        this.year = year;
-    }
-
+    /**
+     * Refleja la duracion (en tipo float) de la cancion al agregarlo en el
+     * arrayList
+     *
+     * @return el duracion de la cancion
+     */
     public float getDuration() {
         return duration;
     }
 
+    /**
+     * Refleja la duracion de la cancion, en caso de ser seteado por usuario
+     *
+     * @param duration la duracion de la cancion asignado por usuario
+     */
     public void setDuration(float duration) {
         this.duration = duration;
     }
 
+    /**
+     * Refleja el genero de la cancion al agregarlo en el arrayList
+     *
+     * @return el genero de la cancion
+     */
     public String getGender() {
         return gender;
     }
 
+    /**
+     * Refleja el genero de la cancion, en caso de ser seteado por usuario
+     *
+     * @param gender el genero de la cancion asignado por usuario
+     */
     public void setGender(String gender) {
         this.gender = gender;
     }
 
+    /**
+     * Refleja la portada de la cancion al agregarlo en el arrayList
+     *
+     * @return la portada de la cancion
+     */
     public String getCover() {
         return cover;
     }
 
+    /**
+     * Refleja la portada de la cancion, en caso de ser seteado por usuario
+     *
+     * @param cover la portada de la cancion asignado por usuario
+     */
     public void setCover(String cover) {
         this.cover = cover;
     }
 
+    /**
+     * Refleja la descripcion de la cancion al agregarlo en el arrayList
+     *
+     * @return la descripcion de la cancion
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * Refleja la descripcion de la cancion, en caso de ser seteado por usuario
+     *
+     * @param description la descripcion de la cancion asignado por usuario
+     */
     public void setDescription(String description) {
         this.description = description;
     }
 
+    /**
+     * [Con este metodo se sobreescribe el metodo toString para asi cuando se
+     * pida el arrayList completo (en este caso myLibrary) Aparezca en pantalla
+     * con el formato especificado
+     *
+     * @return Un orden designado en cuanto se pida imprimir la lista completa
+     *
+     * @author [Juan Esteban, Velasquez Posada ]
+     *
+     * @since [1.0.000 2022-02-10]
+     *
+     */
     @Override
     public String toString() {
-        return "Info [artist= " + artist + ", title= " + title + ", songId= "
-                + songId + ", year=" + year + ", duration= " + duration + ", gender= "
-                + gender + ", cover= " + cover + ", description= " + description + "]" + "\n";
-    }  
+        return "Info [Id=" + songId + ", titulo= " + title + ", Artista= "
+                + artist + ", año= " + year + ", duracion= " + duration + ", genero= "
+                + gender + ", portada= " + cover + ", descripcion= " + description + "]" + "\n";
+    }
 
-    @Override
-    public int compareTo(Info o) {
-        return year.compareTo(o.getYear());
-        
-    }  
-    
 }
--- library/Main.java
+++ library/Main.java
@@ -1,6 +1,10 @@
 package library;
 
+import library.OrderYear;
+import library.InfoLibrary;
+import java.util.Date;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
@@ -12,40 +16,116 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        List<Info> infoList = new ArrayList<>();
-        infoList.add(new Info("muse", "paradise", 1, 2015, 2.11f, "reggae", "memories.jpg", "Cancion 1 del album memories"));
-        infoList.add(new Info("metallica", "resistance", 2, 2015, 1.11f, "reggae", "memories.jpg", "Cancion 2 del album memories"));
-        infoList.add(new Info("lamda", "madness", 3, 2003, 3.12f, "rock", "memories.jpg", "Cancion 3 del album memories"));
-        infoList.add(new Info("muse", "knights", 4, 2001, 4.11f, "punk", "memories.jpg", "Cancion 4 del album memories"));
-        infoList.add(new Info("muse", "knights", 4, 2001, 4.11f, "jazz", "memories.jpg", "Cancion 4 del album memories"));
-        String[] genderList = {"rock", "punk", "jazz", "reggae"};
-
-        int opcion1;
         Scanner scanner = new Scanner(System.in);
-        int opcion2;
-//        System.out.println("De que año deseas traer musica? Ingresa el numero el año. desde 2000 hasta 2022");
-//        opcion1 = scanner.nextInt(); 
-//        infoList.stream().filter(l -> l.getYear() == opcion1).forEach(System.out::println);
-//        System.out.println("****************");
-        
-        System.out.println("Que genero deseas filtrar: 1)rock 2)punk 3) jazz 4)reggae. Ingresa el numero correspondiente");
-        opcion2 = scanner.nextInt();
-        infoList.stream().filter(l -> l.getGender() == genderList[opcion2 - 1]).forEach(System.out::println);
+        int opcion;
+        int opcionGender;
+        int opcionYear;
+        int opcionOrder;
+        int optionOderderMajor;
+        int opcionOrderDate;
+        int opcionPlayList;
 
-//        int opcion3;
-//
-//        Collections.sort(infoList);
-//        for (Info elemento : infoList) {
-//            System.out.println(elemento);
-//
-//        }
-//
-//        System.out.println("****************");
-//
-//        Collections.sort(infoList, Collections.reverseOrder()); // mayor a menor duracion
-//        for (Info elemento : infoList) {
-//            System.out.println(elemento);
-//        }
+        ArrayList<InfoLibrary> myLibrary = new ArrayList<>();
+        InfoLibrary s1 = new InfoLibrary(1, "one", "metallica", 1998, 1.11f, "metal",
+                "And Justice for All.jpg", "Cancion 2 del album memories");
+        myLibrary.add(s1);
+        InfoLibrary s2 = new InfoLibrary(2, "Lose Yourself", "Eminem", 2002, 1.11f, "Rap",
+                "8 Mile.jpg", "Cancion 1 del album 8 Mile");
+        myLibrary.add(s2);
+        InfoLibrary s3 = new InfoLibrary(3, "situation", "escape the fate", 2008, 3.12f, "rock",
+                "DyingIsYourLatestFashion.jpg", "Cancion 3 del album memories");
+        myLibrary.add(s3);
+        InfoLibrary s4 = new InfoLibrary(4, "Dance macabre", "ghost", 2018, 4.20f, "punk",
+                "prequelle.jpg", "Cancion 4 del album memories");
+        myLibrary.add(s4);
+        InfoLibrary s5 = new InfoLibrary(5, "Psychosocial", "slipknot", 2008, 4.11f, "metal",
+                "AllHopeIsGone.jpg", "Cancion 4 del album memories");
+        myLibrary.add(s5);
+        String[] genderList = {"rock", "punk", "metal", "Rap"};
 
+        ArrayList<InfoLibrary> myPlay = new ArrayList<>();
+
+        System.out.println("Que deseas hacer? 1) crear playlist 2)filtrar canciones "
+                + "de la lista de reproduccion completa. 3) organizar lista de mayor a menor "
+                + "4) mostrar libreria completa" + "\n" + " Ingresa el numero de la opcion ");
+        opcion = scanner.nextInt();
+        switch (opcion) {
+            case 1:
+                System.out.println(myLibrary);
+                System.out.println("selecciona ID de la cancion");
+                opcionPlayList = scanner.nextInt();
+                myPlay.add(myLibrary.get(opcionPlayList - 1));
+                System.out.println(myLibrary);
+                System.out.println("selecciona ID de la cancion o muestra playlist nueva");
+                opcionPlayList = scanner.nextInt();
+                myPlay.add(myLibrary.get(opcionPlayList - 1));
+                System.out.println(myPlay);
+
+                break;
+            case 2:
+                System.out.println("Que tipo de filtro deseas: 1) por año "
+                        + "2) por genero musical." + "\n" + " Ingresa el numero de la opcion.");
+                int opcionFilter;
+                opcionFilter = scanner.nextInt();
+                if (opcionFilter == 1) {
+                    System.out.println("De que año deseas traer musica? "
+                            + "\n" + " Ingresa el numero el año. desde 2000 hasta 2022");
+                    opcionYear = scanner.nextInt();
+                    myLibrary.stream().filter(l -> l.getYear() == opcionYear).forEach(System.out::println);
+                } else if (opcionFilter == 2) {
+                    System.out.println("Que genero deseas filtrar: 1)rock 2)punk 3) metal. "
+                            + "\n" + " Ingresa el numero correspondiente");
+                    opcionGender = scanner.nextInt();
+                    myLibrary.stream().filter(l -> l.getGender() == genderList[opcionGender - 1]).forEach(System.out::println);
+                }
+            case 3:
+                System.out.println("Desear organizar por: 1) duracion de la cancion "
+                        + "2) fecha. " + "\n" + " Ingresa el numero correspondiente");
+                opcionOrder = scanner.nextInt();
+                if (opcionOrder == 1) {
+                    System.out.println("Desear organizar de: 1)Por menor duracion de la cancion "
+                            + "2)Por mayor duracion de la cancion. " + "\n" + " Ingresa el numero correspondiente");
+                    optionOderderMajor = scanner.nextInt();
+                    switch (optionOderderMajor) {
+                        case 1:
+                            Collections.sort(myLibrary);
+                            for (Info elem : myLibrary) {
+                                System.out.println(elem);
+                            }
+                            break;
+                        case 2:
+                            Collections.sort(myLibrary, Collections.reverseOrder()); // mayor a menor duracion
+                            for (Info elem : myLibrary) {
+                                System.out.println(elem);
+                            }
+                            break;
+                        default:
+                            System.out.println("ups, esa no era una opcion correcta, comienza de nuevo por favor");
+                    }
+                } else if (opcionOrder == 2) {
+                    System.out.println("Desear organizar de: 1)Por fecha mas actual  "
+                            + "2)Por fecha mas antigua." + "\n" + " Ingresa el numero correspondiente");
+                    opcionOrderDate = scanner.nextInt();
+                    switch (opcionOrderDate) {
+                        case 1:
+                            Collections.sort(myLibrary, new OrderYear());
+                            for (Info year : myLibrary) {
+                                System.out.println(year);
+                            }
+                            break;
+                        case 2:
+                            Collections.sort(myLibrary, Collections.reverseOrder()); // mayor a menor duracion
+                            for (Info elem : myLibrary) {
+                                System.out.println(elem);
+                            }
+                            break;
+                        default:
+                            System.out.println("ups, esa no era una opcion correcta, comienza de nuevo por favor");
+                    }
+
+                }
+            case 4:
+                System.out.println(myLibrary);
+        }
     }
 }
